60bbb7b1cd64818e8dc8af4f9d2b85a66346a6e2
Sumiran Aggarwal <suaggar@microsoft.com>
2019-12-19 15:03:45 +0530

    consumeing new package

--- src/taskparameters.ts
+++ src/taskparameters.ts
@@ -1,9 +1,8 @@
 import * as core from '@actions/core';
 
-import { AzureResourceFilterUtility } from "pipelines-appservice-lib/lib/RestUtilities/AzureResourceFilterUtility";
-import { IAuthorizationHandler } from "pipelines-appservice-lib/lib/ArmRest/IAuthorizationHandler";
-import { Package } from 'pipelines-appservice-lib/lib/Utilities/packageUtility';
-import { getHandler } from 'pipelines-appservice-lib/lib/AuthorizationHandlerFactory';
+import { AzureResourceFilterUtility } from "azure-actions-appservice-rest/Utilities/AzureResourceFilterUtility";
+import { IAuthorizer } from "azure-actions-webclient/Authorizer/IAuthorizer";
+import { Package } from 'azure-actions-utility/packageUtility';
 
 export class TaskParameters {
     private static taskparams: TaskParameters;
@@ -11,23 +10,23 @@ export class TaskParameters {
     private _package: Package;
     private _resourceGroupName?: string;
     private _kind: string;
-    private _endpoint: IAuthorizationHandler;
+    private _endpoint: IAuthorizer;
     private _publishProfileContent: string;
     private _slotName: string;
 
-    private constructor() {
+    private constructor(endpoint: IAuthorizer) {
         this._publishProfileContent = core.getInput('publish-profile');
         this._package = new Package(core.getInput('package', { required: true }));
         if(!this._publishProfileContent) {
-            this._endpoint = getHandler();
+            this._endpoint = endpoint;
             this._appName = core.getInput('app-name', {required: true});
             this._slotName = core.getInput('slot-name');
         }
     }
 
-    public static getTaskParams() {
+    public static getTaskParams(endpoint?: IAuthorizer) {
         if(!this.taskparams) {
-            this.taskparams = new TaskParameters();
+            this.taskparams = new TaskParameters(!!endpoint ? endpoint : null);
         }
         return this.taskparams;
     }
