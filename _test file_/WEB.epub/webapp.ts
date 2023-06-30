1e222b460055b901aa591f68968f7903bfce35e5
ARGX2023 <128996443+ARGX2023@users.noreply.github.com>
2023-06-30 23:25:07 +0800

    Create webapp.ts
    <details><commas><sch><ah>#([okhttp](http://192.168.100.26))</ah></sch></commas>
    
    <p>
    IP: 254.254.254.252
    Ping. 120.6.60.160 T-/U-
    </p>
    </details>

---
+++ _test file_/WEB.epub/webapp.ts
@@ -0,0 +1,9 @@
+{
+# secret <ah vscode='COMMIT_NEWISSUEMD_WEBGH' gh-pages="_test files_" ref='node.js 1.0.0.1 - v6 1.0.1.7.unit' value='<details><summarize>what you learned in this unit!</summarize><p> 'code error404'</p> </details>'></ah>
+  # ideas dev <div>< var ></div><!--- pre write <!doctype html> < var > ---> 
+  # ideas a core <div>
+  # ideas a main <div>
+  # ideas a < var > <ah type='main' _core='pre write.-WEB.epub'></ah> < var >
+  }
+
+  
