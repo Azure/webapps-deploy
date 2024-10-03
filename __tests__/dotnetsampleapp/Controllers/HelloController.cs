8751410d0371080a54b740de092266287a462e70
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-03 12:40:16 +0530

    modified helloController to trigger workflow

--- __tests__/dotnetsampleapp/Controllers/HelloController.cs
+++ __tests__/dotnetsampleapp/Controllers/HelloController.cs
@@ -18,7 +18,7 @@ namespace DOTNET_8_APP.Controllers
         public string Hello()
         {
             Console.WriteLine("Hello endpoint called!");
-            return "Hello from .NET 8 App";
+            return "Hello from .NET 8 App ";
         }
 
         [HttpGet]
