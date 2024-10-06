0d666ebb6ea79ea4bb75f24310dfe32416a507fa
Saipriya-1144 <saipriyach1144@gmail.com>
2024-10-06 12:09:04 +0530

    added node sample app

---
+++ __tests__/nodesampleapp/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    console.log('Hello endpoint called!');
+    res.send('Hello from NodeJs App, Heyyy holaaa!')
+})
+
+app.get('/dummy', (req, res) => {
+    console.log('Dummy endpoint called!');
+    var s = process.env.DUMMY_APPSETTING || "Appsetting not Found";
+    res.send(s)
+})
+
+app.get('/placeholder', (req, res) => {
+    console.log('Placeholder endpoint called!');
+    res.send('<<<node-place-holder>>>');
+})
+
+app.listen(port, () => console.log('Listening on port %s', port));
