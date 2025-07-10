6208b1e99bbce9111988ab2f5357760f15222599
Akshay Kumar <kumaraksh@microsoft.com>
2025-07-10 21:31:11 +0530

    adding logs

--- src/DeploymentProvider/Providers/WebAppSiteContainersDeploymentProvider.ts
+++ src/DeploymentProvider/Providers/WebAppSiteContainersDeploymentProvider.ts
@@ -1,5 +1,6 @@
 import { BaseWebAppDeploymentProvider } from './BaseWebAppDeploymentProvider';
 import { SiteContainerDeploymentUtility } from 'azure-actions-appservice-rest/Utilities/SiteContainerDeploymentUtility';
+import * as core from '@actions/core';
 
 export class WebAppSiteContainersDeploymentProvider extends BaseWebAppDeploymentProvider {
     public async DeployWebAppStep() {
@@ -8,6 +9,7 @@ export class WebAppSiteContainersDeploymentProvider extends BaseWebAppDeployment
 
         for (let i = 0; i < siteContainers.length; i++) {
             let siteContainer = siteContainers[i];
+            core.info("updating site container: " + JSON.stringify(siteContainer));
             await siteContainerDeploymentUtility.updateSiteContainer(siteContainer);
         }
     }
