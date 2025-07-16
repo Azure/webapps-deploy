78f625899bc83a599ff9afb1f1f6a1cb0bf429a6
kumaraksh1 <112485097+kumaraksh1@users.noreply.github.com>
2025-07-16 14:43:04 +0530

    Use a zip archival utility to exclude some file patterns (#465)
    * Use new archival method
    
    * update ziputility archival method

--- src/DeploymentProvider/Providers/WebAppDeploymentProvider.ts
+++ src/DeploymentProvider/Providers/WebAppDeploymentProvider.ts
@@ -39,7 +39,8 @@ export class WebAppDeploymentProvider extends BaseWebAppDeploymentProvider {
     
                 case PackageType.folder:
                     let tempPackagePath = utility.generateTemporaryFolderOrZipPath(`${process.env.RUNNER_TEMP}`, false);
-                    webPackage = await zipUtility.archiveFolder(webPackage, "", tempPackagePath) as string;
+                    // exluding release.zip while creating zip for deployment.
+                    webPackage = await zipUtility.archiveFolderWithExcludePatterns(webPackage, "", tempPackagePath, ['release.zip']) as string; 
                     core.debug("Compressed folder into zip " +  webPackage);
                     core.debug("Initiated deployment via kudu service for webapp package : "+ webPackage);
                     this.actionParams.type = "zip";
