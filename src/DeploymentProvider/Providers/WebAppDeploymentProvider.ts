735efb05178239cc1f1d21ba34aca18ca1e95365
kumaraksh1 <112485097+kumaraksh1@users.noreply.github.com>
2025-07-18 18:49:11 +0530

    Revert "releases/v2 | minor patch release | Use a zip archival utility to exc…" (#471)
    This reverts commit 1360898b192cf92e567b3f7e690f84f36bba9a3f.

--- src/DeploymentProvider/Providers/WebAppDeploymentProvider.ts
+++ src/DeploymentProvider/Providers/WebAppDeploymentProvider.ts
@@ -36,8 +36,7 @@ export class WebAppDeploymentProvider extends BaseWebAppDeploymentProvider {
 
             case PackageType.folder:
                 let tempPackagePath = utility.generateTemporaryFolderOrZipPath(`${process.env.RUNNER_TEMP}`, false);
-                // exluding release.zip while creating zip for deployment.
-                webPackage = await zipUtility.archiveFolderWithExcludePatterns(webPackage, "", tempPackagePath, ['release.zip']) as string;
+                webPackage = await zipUtility.archiveFolder(webPackage, "", tempPackagePath) as string;
                 core.debug("Compressed folder into zip " +  webPackage);
                 core.debug("Initiated deployment via kudu service for webapp package : "+ webPackage); 
                 this.deploymentID = await this.kuduServiceUtility.deployUsingZipDeploy(webPackage, { slotName: this.actionParams.slotName, commitMessage: this.actionParams.commitMessage });
