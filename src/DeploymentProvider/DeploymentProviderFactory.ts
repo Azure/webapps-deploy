fb383a8b56450a2e07369c46785cba3c1c468087
Akshay Kumar <kumaraksh@microsoft.com>
2025-07-29 15:32:24 +0530

    test

--- src/DeploymentProvider/DeploymentProviderFactory.ts
+++ src/DeploymentProvider/DeploymentProviderFactory.ts
@@ -1,3 +1,5 @@
+import * as core from '@actions/core';
+
 import { ActionParameters, WebAppKind } from "../actionparameters";
 
 import { DEPLOYMENT_PROVIDER_TYPES } from "./Providers/BaseWebAppDeploymentProvider";
@@ -20,15 +22,19 @@ export class DeploymentProviderFactory {
         }
         else if(type == DEPLOYMENT_PROVIDER_TYPES.SPN) {
             if (!!ActionParameters.getActionParams().blessedAppSitecontainers) {
+                core.info("Using blessed app site containers deployment provider");
                 return [new WebAppDeploymentProvider(type), new WebAppSiteContainersDeploymentProvider(type)];
             }
             else if (!!ActionParameters.getActionParams().siteContainers) {
+                core.info("Using site containers deployment provider");
                 return [new WebAppSiteContainersDeploymentProvider(type)];
             }
             else if(!!ActionParameters.getActionParams().images || (!!ActionParameters.getActionParams().isLinux && !!ActionParameters.getActionParams().multiContainerConfigFile)) {
+                core.info("Using web app container deployment provider");
                 return [new WebAppContainerDeploymentProvider(type)];
             }
             else {
+                core.info("Using web app deployment provider");
                 return [new WebAppDeploymentProvider(type)];
             }
         }
