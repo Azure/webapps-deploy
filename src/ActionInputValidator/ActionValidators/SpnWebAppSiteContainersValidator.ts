3d5572fa91ad94c46c122ee75acdad4e72567aa9
Akshay Kumar <kumaraksh@microsoft.com>
2025-07-30 15:19:20 +0530

    fix validator

--- src/ActionInputValidator/ActionValidators/SpnWebAppSiteContainersValidator.ts
+++ src/ActionInputValidator/ActionValidators/SpnWebAppSiteContainersValidator.ts
@@ -3,10 +3,17 @@ import { validateSiteContainersInputs } from "../Validations";
 
 import { IValidator } from "./IValidator";
 import { SpnLinuxWebAppValidator } from "./SpnLinuxWebAppValidator";
+import { ActionParameters } from '../../actionparameters';
 
 export class SpnWebAppSiteContainersValidator extends SpnLinuxWebAppValidator {
     async validate(): Promise<void> {
-        await super.validate();
+
+        let actionParams: ActionParameters = ActionParameters.getActionParams();
+
+        if (!!actionParams.blessedAppSitecontainers) {
+            core.info("Blessed site containers detected, using SpnLinuxWebAppValidator for validation.");
+            await super.validate();
+        }
 
         core.info("Validating SPN Web App Site Containers inputs...");
         validateSiteContainersInputs();
